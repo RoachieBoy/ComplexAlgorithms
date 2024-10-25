5d947dd47646a409c82f8cd30c5016ed840e8ad1
Caleigh  Esterhuyse <chuckinmybox@gmail.com>
2024-10-25 17:54:26 +0200

    further expanding graph

--- Assignments/Network Flows/Evacuation/CityNetwork.cs
+++ Assignments/Network Flows/Evacuation/CityNetwork.cs
@@ -2,17 +2,38 @@ namespace Evacuation;
 
 internal class CityNetwork
 {
-    internal int NumberOfCities { get; set; }
-    internal int NumberOfRoads { get; set; }
-    private IList<Road> Roads { get; set; }
+    /// <summary>
+    ///  Number of cities that are contained within the network 
+    /// </summary>
+    private int NumberOfCities { get; }
+    
+    /// <summary>
+    ///  A collection of lists where each index represents a city in the network
+    ///  and each list is made up of roads that connect the city to other cities 
+    /// </summary>
+    private IList<Road>[] AdjacencyList { get; }
 
-    internal CityNetwork(int numberOfCities, int numberOfRoads)
+    internal CityNetwork(int numberOfCities)
     {
         NumberOfCities = numberOfCities;
-        NumberOfRoads = numberOfRoads; 
-        Roads = new List<Road>();
+        AdjacencyList = new IList<Road>[NumberOfCities + 1];
+
+        // Fill the adjacency list with the necessary amount of roads 
+        for (var i = 0; i <= numberOfCities; i++)
+        {
+            AdjacencyList[i] = new List<Road>();
+        }
     }
 
-    internal void InitialiseRoad(int startPoint, int endPoint, int hourlyCapacity) 
-            => Roads.Add(new Road(startPoint, endPoint, hourlyCapacity));
+    internal void InitialiseRoad(int startPoint, int endPoint, int hourlyCapacity, IList<Road> roads)
+    {
+        var forwardRoad = new Road(startPoint, endPoint, hourlyCapacity);
+        // Capacity has a starting value of 0 because you can't go backwards before you have gone forwards 
+        var backwardRoad = new Road(endPoint, startPoint, 0);
+        
+        // Represents the path forward from the starting city to the main city
+        AdjacencyList[startPoint].Add(forwardRoad);
+        // Represents the path reversed from the starting city to the main city
+        AdjacencyList[endPoint].Add(backwardRoad);
+    }
 }
--- Assignments/Network Flows/Evacuation/Road.cs
+++ Assignments/Network Flows/Evacuation/Road.cs
@@ -2,10 +2,31 @@ namespace Evacuation;
 
 internal class Road
 {
-    internal int StartPoint { get; private set; }
-    internal int EndPoint { get; private set; }
-    internal int HourlyCapacity{ get; private set; }
+    /// <summary>
+    ///  The point on the graph from which the road begins
+    /// </summary>
+    private int StartPoint { get; set; }
+    
+    /// <summary>
+    ///  The point on the graph where the road ends
+    /// </summary>
+    private int EndPoint { get; set; }
+    
+    /// <summary>
+    ///  The maximum capacity a road can handle at any given hour 
+    /// </summary>
+    private int HourlyCapacity{ get; }
+    
+    /// <summary>
+    ///  The current flow of people on the road 
+    /// </summary>
+    internal int CurrentFlow { get; }
 
+    /// <summary>
+    ///  The remaining capacity still left on the road 
+    /// </summary>
+    internal int RemainingCapacity => HourlyCapacity - CurrentFlow;
+    
     internal Road(int startPoint, int endPoint, int hourlyCapacity)
     {
         StartPoint = startPoint;
