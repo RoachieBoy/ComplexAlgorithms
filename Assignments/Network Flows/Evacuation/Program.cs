471b04bfdda61529d3084a504cb0b45c39fc0674
Caleigh  Esterhuyse <chuckinmybox@gmail.com>
2024-11-03 18:36:27 +0100

    added second test

--- Assignments/Network Flows/Evacuation/Program.cs
+++ Assignments/Network Flows/Evacuation/Program.cs
@@ -20,3 +20,20 @@ stopwatch.Stop();
 
 Console.WriteLine(maxFlow);
 Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Reset();
+stopwatch.Start();
+
+var testGraph2 = new Graph<int>(4);
+testGraph2.AddEdge(0, 1, 10000);
+testGraph2.AddEdge(0, 2, 10000);
+testGraph2.AddEdge(1, 2, 1);
+testGraph2.AddEdge(1, 3, 10000);
+testGraph2.AddEdge(2, 3, 10000);
+
+var maxFlow2 = MaxFlowUtilities.FindMaxFlow(testGraph2, 0, 3, MaxFlowAlgorithmTypes.EdmondKarp);
+
+Console.WriteLine(maxFlow2);
+Console.WriteLine($"Time taken second test: {stopwatch.ElapsedMilliseconds} ms");
+
+
